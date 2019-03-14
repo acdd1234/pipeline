b5c0f3033373e0b67478738fe12e2cd232ee66ac
acdd1234 <amadoucd@gmail.com>
2019-03-14 14:59:28 -0400

    Revert "test21"
    This reverts commit 5e8f51be0b95597aaf6c5ea35da869c1040c8eca.

--- git/src/test/TestMyClass.java
+++ git/src/test/TestMyClass.java
@@ -5,7 +5,7 @@ public class TestMyClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.println("test3");
-       System.out.println("test321");
+       System.out.println("test32");
 	}
 
 }
