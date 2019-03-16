193c6f07c4d6fbadee4935584cbb0c40dea9d919
acdd1234 <amadoucd@gmail.com>
2019-03-16 16:51:08 -0400

    # WARNING: head commit changed in the meantime Test

---
+++ MyFirstPipeline/src/main/java/MyFirstPipeline/MyFirstPipeline/App.java
@@ -0,0 +1,13 @@
+package MyFirstPipeline.MyFirstPipeline;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
